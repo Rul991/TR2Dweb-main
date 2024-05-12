218a48440bd960b85a19377ecbfa4a089d66bf5b
Rul <archerartem6@gmail.com>
2024-05-12 19:39:30 +0300

    deleted:    index.js 	deleted:    package-lock.json 	deleted:    package.json

--- index.js
+++
@@ -1,7 +0,0 @@
-const phaser = require('phaser');
-const fs = require('fs');
-const express = require('express');
-const { log } = require('console');
-
-const app = express();
-const game = new phaser.Game();
